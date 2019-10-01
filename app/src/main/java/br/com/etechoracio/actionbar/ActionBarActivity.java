bfa0365d1461e859eceec6df41ad1b75c7dac276
Feliperony <rony.alvessobral@hotmail.com>
2019-09-30 22:28:17 -0300

    Criação da ActionBar

--- app/src/main/java/br/com/etechoracio/actionbar/ActionBarActivity.java
+++ app/src/main/java/br/com/etechoracio/actionbar/ActionBarActivity.java
@@ -2,6 +2,10 @@ package br.com.etechoracio.actionbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ActionBarActivity extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class ActionBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_bar2);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_wifi:
+                Toast.makeText(this, "Wi-fi Acionado!", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_bluetooth:
+                Toast.makeText(this, "Bluetooth Acionado!", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_sobre:
+                Toast.makeText(this, "Sobre Acionado!", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_sair:
+                finish();
+                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+
+        }
     }
 }
